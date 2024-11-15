7c261b1489ad05d12e4becf8078dec5c535a5d4d
Valerie Johnson <valeriecrist@gmail.com>
2024-11-15 17:19:49 -0600

    Final submission

--- .env/Scripts/activate.bat
+++ .env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Valer\Desktop\WK4-JSON\.env
+set VIRTUAL_ENV=C:\Users\Valer\Desktop\Lesson01\WK4-JSON\.env
 
 if not defined PROMPT set PROMPT=$P$G
 
